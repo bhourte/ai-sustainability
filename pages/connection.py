aefa0d5aa03c7309df72607fa2e287ca308fa6a4
hennecarta <alexandre.hennecart@delaware.pro>
2023-03-30 10:44:21 +0200

    ajout page connection + variable global username

---
+++ pages/connection.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+def main():
+    st.title("Connection")
+    
+    if 'username' not in st.session_state:
+        st.session_state.username = st.text_input("Put your username here to connect :", "none")
+    else:
+         st.session_state.username = st.text_input("Put your username here to connect :", st.session_state.username)
+
+if __name__ == "__main__":
+      main()
--- pages/historic.py
+++ pages/historic.py
@@ -1,6 +1,10 @@
 import streamlit as st
 
 def main():
+    if 'username' in st.session_state:
+        st.caption("Connected as " + str(st.session_state.username))
+    else:
+        st.caption("You are not connected, please connect with your username in the Connection page")
     st.title("Historic")
 
 if __name__ == "__main__":
