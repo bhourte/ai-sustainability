ac5eb3dc41261e24fabc5daab9b5318219440d3f
hennecarta <alexandre.hennecart@delaware.pro>
2023-03-30 15:27:11 +0200

    ajout historic
    ajout de l'historique, mais sans l'option qui modifie le graph enregistré, c'est un nouvau qui l'est à la place et l'ancien est toujours là

--- pages/historic.py
+++ pages/historic.py
@@ -3,6 +3,28 @@ from class_form import Form
 from decouple import config
 
 
+FIRST_NODE_ID = '1'
+BASE_MODIF_CRYPTED = False
+
+@st.cache_resource
+def get_list_result(_form, node_answer, next_node_id):
+
+    # We create a list with all previous answers
+    previous_answers = []
+    while next_node_id != 'end' and next_node_id is not None:
+        node_answer_label = _form.run_gremlin_query("g.V('"+str(node_answer)+"').properties('label')")[0]['value']
+        if node_answer_label == 'end':
+            break
+        next_node_id = _form.run_gremlin_query("g.V('"+str(node_answer)+"').properties('question_id')")[0]['value']
+        panswer = _form.run_gremlin_query("g.V('"+str(node_answer)+"').outE().properties('answer')")
+        previous_answer = []
+        for i in panswer:
+            previous_answer.append(i['value'])
+        previous_answers.append(previous_answer)
+
+        node_answer = _form.run_gremlin_query("g.V('"+str(node_answer)+"').out().properties('id')")[0]['value']  # We go to the next vertice
+    return previous_answers
+
 def main():
     st.title("Historic")
     if 'username' not in st.session_state or st.session_state.username == "":
@@ -17,6 +39,33 @@ def main():
             container_name = 'Form',
             primary_key= config('PRIMARYKEY'),
         )
+
+    node_answer = form.add_qcm_select_form(username)
+    if node_answer == None:  # if non form selected, don't show the rest
+        return None
+    next_node_id = node_answer
+
+    previous_answers = get_list_result(form, node_answer, next_node_id)
+
+    next_node_id, answer, modif_crypted = form.add_question(FIRST_NODE_ID, BASE_MODIF_CRYPTED, previous_answers[0])
+    answers = [answer]
+    i = 1
+    while next_node_id != '9' and next_node_id is not None:
+        next_node_id, answer, modif_crypted = form.add_question(next_node_id, modif_crypted, previous_answers[i])
+        if answer is not None:
+            answers.append(answer)
+            if previous_answers[i] != None and answer[0]['text'] != previous_answers[i][0]:
+                previous_answers = [None] * len(previous_answers)
+        i += 1
+
+    if next_node_id == '9':
+
+        print(form.calcul_best_AIs(5, answers))
+
+        if st.button('Submit', on_click=form.save_answers, args=(answers,username)):
+            st.write('Answers saved')
+            st.write(answers)
+
 
 if __name__ == "__main__":
       main()
