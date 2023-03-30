9d4d9de5efab830828de3ff7b2befc36f29d9dd9
ArnauldFilee <ArnauldFilee@users.noreply.github.com>
2023-03-30 15:33:02 +0200

    stats beginning

--- pages/historic.py
+++ pages/historic.py
@@ -14,7 +14,7 @@ def main():
     form = Form(
             endpoint = "questions-db.gremlin.cosmos.azure.com",
             database_name = "graphdb",
-            container_name = 'Form',
+            container_name = 'Persons',
             primary_key= config('PRIMARYKEY'),
         )
 
--- pages/statistic.py
+++ pages/statistic.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from class_form import Form
+from decouple import config
 
 def main():
     st.title("Statistic")
@@ -7,6 +9,15 @@ def main():
         return None
     username = st.session_state.username
     st.caption("Connected as " + str(username))
+    form = Form(endpoint="questions-db.gremlin.cosmos.azure.com", database_name="graphdb", container_name='Persons', primary_key= config('PRIMARYKEY'),)
+    if username != 'Admin':
+        st.write("You are not an Admin")
+        st.write("You can't access to this page")
+    else:
+        st.write("Welcome Admin")
+        st.write("You can now see the statistic of the form")
+        selected_edges = form.get_nb_selected_edges()
+        form.display_bar_graph(selected_edges)
 
 if __name__ == "__main__":
-      main()
+    main()
