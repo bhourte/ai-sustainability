67815f2dbde28a52d579bb94b9806f0d1d066f62
ArnauldFilee <ArnauldFilee@users.noreply.github.com>
2023-03-31 14:58:26 +0200

    good luck with new file

--- connection.py
+++ connection.py
@@ -1,12 +1,14 @@
 import streamlit as st
-
+from streamlit.source_util import get_pages
 def main():
     st.title("Connection")
-    
     if 'username' not in st.session_state:
         st.session_state.username = st.text_input("Put your username here to connect :")
+        
+        
     else:
          st.session_state.username = st.text_input("Put your username here to connect :", st.session_state.username)
-
+         st.sidebar.write("Connected as " + str(st.session_state.username))
+    
 if __name__ == "__main__":
     main()
