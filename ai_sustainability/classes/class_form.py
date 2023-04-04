2c6d75f2c1b3f50cee72adab99d2c382bbeccb52
ArnauldFilee <ArnauldFilee@users.noreply.github.com>
2023-04-04 17:13:43 +0200

    class_organization

--- ai_sustainability/classes/class_form.py
+++ ai_sustainability/classes/class_form.py
@@ -6,4 +6,16 @@ methods:
     - display_question
     - display_answer
     - ...
+
+    ONE QUESTION :
+    return : dict {
+            1: question_text
+            2: answers
+            3: help_text
+            4: question_label
+        }
+    
+    A chaque réponse de l'utilisateur, retourner le texte de celle-ci ainsi que les anciennes réponses (list(str))
+    Pour Q_QRM, list dans list (list(list(str)))
+    Pour Q_NEXT, Q_QCM, str answer    
 """
--- ai_sustainability/classes/db_connection.py
+++ ai_sustainability/classes/db_connection.py
@@ -1,3 +1,30 @@
 """
 Make the connection to the database, run the querys and close the connection
+
+1)
+
+    get_first_question() : get the first question of the form
+    get_one_question() : 
+        - get_question_text() : get the text of the question (str) with actual_question
+        - get_answers_text() list of all the answers (str) with actual_question
+        - get_help_text() : get the help text (str) with actual_question
+
+        return : dict {
+            1: text question
+            2: list of answers
+            3: help text
+            4: Label question
+        }
+    save_answer() : save the answer in the database
+
+    give answer(answer_list) : following the len of the list, we can find the question by the answer
+
+    attributes :
+        - list_questions (list) : list of past questions and last is actual_question (ids db)
+    
+        
+
+
+
+
 """
