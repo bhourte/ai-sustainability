5a1a2e705d4c8ca15a85af33a2f6b37d9545c749
ArnauldFilee <ArnauldFilee@users.noreply.github.com>
2023-04-04 09:29:49 +0200

    clean code connexion

--- 👤_connection.py
+++ 👤_connection.py
@@ -4,35 +4,30 @@ launch it with: streamlit run 👤_connection.py
 """
 import streamlit as st
 
-
 def main():
     """
         This is the code used to show the "Connection" page
         The user will connect here and be able to acces to the rest of the application after that
     """
+
     st.set_page_config(page_title="Connection Page", page_icon="👤")
     st.title("👤Connection")
     if 'username' not in st.session_state:  # User not already connected
         username = st.text_input("Put your username here to connect :")
+    else: # User already connected, but can switch
+        username = st.text_input("Put your username here to connect :", st.session_state.username)
+
+    if username != "":  # User connected
         if '-' in username:
             st.warning("You can't use '-' in your username")
-        else:
+        else :
+            if username == 'Admin':
+                st.caption("🔑Connected as an " + str(username))
+            else :
+                st.caption("✅Connected as " + str(username))
             st.session_state.username = username
             st.session_state.last_form_name = None  # To detect if the user create a form with the same name as the previous one
             st.session_state.clicked = False
-    else:  # User already connected, but can switch
-        username = st.text_input("Put your username here to connect :", st.session_state.username)
-        if '-' in username:
-            st.warning("You can't use '-' in your username")
-        else:
-            st.session_state.username = username
-            st.session_state.last_form_name = None
-
-    if st.session_state.username != "":  # User connected
-        if st.session_state.username == 'Admin':
-            st.caption("🔑Connected as an " + str(st.session_state.username))
-        else :
-            st.caption("✅Connected as " + str(st.session_state.username))
     else:
         st.caption("❌Not connected")
 
