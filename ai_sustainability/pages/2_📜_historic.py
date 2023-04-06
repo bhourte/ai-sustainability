fa2905e71e821a14f52f8a31f6bb8c37b39679fe
hennecarta <alexandre.hennecart@delaware.pro>
2023-04-06 08:50:08 +0200

    commit pour pull

--- ai_sustainability/pages/2_📜_historic.py
+++ ai_sustainability/pages/2_📜_historic.py
@@ -195,10 +195,102 @@ def main_new() -> None:
     """
 
     database = DbConnection()
-    st_form = HistoricStreamlit(database)
-    username = st_form.username
+    st_historic = HistoricStreamlit(database)
+    username = st_historic.username
     if not username:
         return
+
+    # Connected as an User
+    if username != "Admin":
+        list_answered_form = database.get_all_forms(username)
+        selected_form = st_historic.show_choice_form(list_answered_form)
+        if selected_form:  # if none form selected, don't show the rest
+            return
+        form_name = node_answer.split("-")[-1]
+        next_node_id = node_answer
+
+        previous_answers = get_list_result(
+            form, node_answer
+        )  # get the list with all previous answers contained in the form
+
+        next_node_id, answer, modif_crypted = form.add_question(FIRST_NODE_ID, BASE_MODIF_CRYPTED, previous_answers[0])
+        label_next_node = form.run_gremlin_query("g.V('" + str(next_node_id) + "').properties('label')")[0]["value"]
+        answers = [answer]
+        i = 1
+        while label_next_node != "end" and next_node_id is not None:
+            next_node_id, answer, modif_crypted = form.add_question(next_node_id, modif_crypted, previous_answers[i])
+            if answer is not None and len(answer) != 0:
+                label_next_node = form.run_gremlin_query("g.V('" + str(next_node_id) + "').properties('label')")[0][
+                    "value"
+                ]
+                answers.append(answer)
+                if previous_answers[i] is not None and answer[0]["text"] != previous_answers[i][0]:
+                    previous_answers = [None] * len(previous_answers)
+            i += 1
+            if i == len(previous_answers):
+                previous_answers.append(None)
+
+        # If the form is not finish, we continue to show it with a new question
+        if label_next_node != "end":
+            return
+
+        # We ask the user if he want to change the name of his form
+        new_form_name = st.text_input("If you want to change the name of the form, change it here:", form_name)
+        if new_form_name == "":  # The name can not be empty
+            return
+
+        if not form.no_dash_in_my_text(new_form_name):  # No - in the name
+            st.warning("The name of the form can't contain a dash.")
+            return
+
+        print(form_name)
+        print(new_form_name)
+        if (
+            form.run_gremlin_query("g.V('" + username + "-answer1-" + new_form_name + "')")
+            and new_form_name != form_name
+        ):
+            st.warning(
+                "You already have a form with this name, please pick an other name or select it above if you want to change it."
+            )
+            return
+
+        list_bests_ais = form.calcul_best_ais(N_BEST_AI, answers)  # get the N best AI (5 for now)
+        form.show_best_ai(list_bests_ais)  # We show the N best AI to the user (5 for now)
+        if st.button(
+            "Save Change",
+            on_click=form.change_answers,
+            args=(answers, username, list_bests_ais, form_name, new_form_name),
+        ):
+            st.session_state.clicked = False  # Put to False to be sure it does not bug in the Form page
+            print("best AIs : " + str(list_bests_ais))
+            st.write("Change saved")
+            st.write(answers)
+
+    # Connected as an Admin
+    else:
+        all_user = ["<Select an User>"] + form.run_gremlin_query("g.V().haslabel('user')")
+        i = 1
+        for i in range(1, len(all_user)):
+            all_user[i] = all_user[i]["id"]
+
+        # The admin select an user
+        user = st.selectbox(label="Select an user", options=all_user)
+        if user == "<Select an User>":
+            return
+
+        all_form = ["<Select a Form>"] + form.run_gremlin_query("g.V('" + str(user) + "').out().haslabel('Answer')")
+        # Shaping of the texts
+        for i in range(1, len(all_form)):
+            all_form[i] = all_form[i]["id"].split("-")[-1]
+            i += 1
+
+        # The admin select a form of the choosen user
+        form_name = str(st.selectbox(label="Select a Form", options=all_form))
+        if form_name == "<Select a Form>":
+            return
+
+        first_node = str(user) + "-answer1-" + str(form_name)
+        show_form(form, first_node)  # We show the form below the selection boxes
 
 
 if __name__ == "__main__":
