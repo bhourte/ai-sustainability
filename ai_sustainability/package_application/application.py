63a8f0b73381df2bd01940763793dbe42d060b0c
hennecarta <alexandre.hennecart@delaware.pro>
2023-04-13 11:07:41 +0200

    doc application.py

--- ai_sustainability/package_application/application.py
+++ ai_sustainability/package_application/application.py
@@ -52,7 +52,7 @@ class Application:
         Get the id of the next question
 
         Parameters :
-            - answer : the answer given by the user for the question (if "" : this means no specific answer eg: Q_Next)
+            - answer : all answers given by the user for the question
 
         Return :
             - a Question corresponding to the next question according to the actual_question and answer provided
@@ -84,11 +84,8 @@ class Application:
         Parameters:
             - nb_ai (int): number of AI to return
             - answers (list): list of the answers of the user
-
-        Return:
-            - list_bests_ais (list): list of the best AI to use
         """
-        list_ai = self.database.get_all_ais()
+        list_ai = self.database.get_all_ais()  # We get all existing AIs
         return self.business.calcul_best_ais(nb_ai=nb_ai, list_ai=list_ai, list_answers=answers)
 
     def get_best_ais(self, username: User, form_name: str) -> list[str]:
@@ -100,32 +97,18 @@ class Application:
     def get_all_users(self) -> list[User]:
         """
         Return all users in the database
-            Return :
-                - result : list of all users (list of str)
         """
         return self.database.get_all_users()
 
     def get_all_forms_names(self, username: User) -> list[str]:
         """
-        Get all names of the forms of a user (in fact, get all the id lol)
-
-        Parameters:
-            - username (str): username of the user
-
-        Return:
-            - list of the forms_id
+        Get all names of the forms of a user
         """
         return self.database.get_all_forms_names(username)
 
     def get_list_answers(self, username: User, selected_form: str) -> AnswersList:
         """
         Get the list of answers of a form
-
-        Parameters:
-            - selected_form (str): id of the form
-
-        Return:
-            - list of the answers
         """
         return self.database.get_list_answers(username, selected_form)
 
@@ -137,32 +120,17 @@ class Application:
 
     def get_nb_selected_edge_stats(self) -> list[SelectedEdge]:
         """
-        Return a dict with number of selected edge for each proposition
-        Especially for stats showing
-        Return :
-            - list of SelectedEdge
+        Return a list with all existing edges and the number of time they had been selected
+        Used in stats showing
         """
         return self.database.get_nb_selected_edge()
 
     def check_user_exist(self, username: User) -> bool:
-        """
-        Check if a user exists in the database
-
-        Parameters :
-            - username : the username of the user (str)
-        """
         return self.database.check_node_exist(username)
 
     def check_form_exist(self, username: User, form_name: str) -> bool:
         """
-        Check if a form exist in the database
-
-        Parameters :
-            - username : the username of the user (str)
-            - form_name : the name of the form (str)
-
-        Return :
-            - bool : True if the form exist, False otherwise
+        Check if a form exist for a specific user in the database
         """
         return self.database.check_node_exist(f"{username}-answer1-{form_name}")
 
@@ -174,9 +142,10 @@ class Application:
             - username (str): username of the user
             - form_name (str): name of the form
             - answers (list): list of the answers of the user
+            - list_best_ai (list[str]): list of the n best AIs selected for this form
 
         Return:
-            - bool: True if the answers are saved, False if the form already exist
+            - bool: True if the answers are well saved, False if the form already exist
         """
         return self.database.save_answers(username, form_name, answers, self.list_questions, list_best_ai)
 
@@ -191,9 +160,10 @@ class Application:
             - username (str): username of the user
             - form_name (str): name of the form
             - new_form_name (str): new name of the form
+            - list_best_ai (list[str]): list of the n best AIs selected for this form
 
         Return:
-            - bool: True if the answers are saved, False if the form already exist
+            - bool: True if the answers are well saved, False if the form already exist
         """
         return self.database.change_answers(
             answers, username, form_name, new_form_name, self.list_questions, list_best_ai
