e80583b9a859fd8ce427b577cca7f7b6ebfb597a
hennecarta <alexandre.hennecart@delaware.pro>
2023-06-02 14:51:58 +0200

    suite

--- ai_validation/0_🔍_Experiment_selection.py
+++ ai_validation/0_🔍_Experiment_selection.py
@@ -19,22 +19,22 @@ class UserInterface:
 
     def select_user(self, list_username: list[str]) -> Optional[str]:
         """Method used to show all user and select one"""
-        list_username = ["<Select a user>", "<All experiments>", "<Only independant experiments>"] + list_username
+        list_username = ["<Select an user>", "<All experiments>", "<Only independant experiments>"] + list_username
         question = "Select an user"
         selected_user = str(st.selectbox(label=question, options=list_username, index=0))
         if selected_user == "<All experiments>":
             return None
-        return selected_user if selected_user != "<Select a user>" else ""
+        return selected_user if selected_user != "<Select an user>" else ""
 
     def select_experiment(self, list_experiment: list[Experiment]) -> Optional[Experiment]:
         """Method used to show all not empty experiment and used to select one"""
         list_exp = [f"{i.experiment_name} with id : {i.experiment_id}" for i in list_experiment]
-        list_exp = ["<Select a experiment>"] + list_exp
-        question = "Select a experiment by is name"
+        list_exp = ["<Select an experiment>"] + list_exp
+        question = "Select an experiment by is name"
         selected_experiment = str(st.selectbox(label=question, options=list_exp, index=0))
         return (
             list_experiment[list_exp.index(selected_experiment) - 1]
-            if selected_experiment != "<Select a experiment>"
+            if selected_experiment != "<Select an experiment>"
             else None
         )
 
