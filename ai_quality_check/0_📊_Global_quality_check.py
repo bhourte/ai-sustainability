55300c56bb4e6f64b82989af24617723e657054e
hennecarta <alexandre.hennecart@delaware.pro>
2023-06-19 15:36:47 +0200

    Modif typecheck and modif text

--- ai_quality_check/0_📊_Global_quality_check.py
+++ ai_quality_check/0_📊_Global_quality_check.py
@@ -3,6 +3,8 @@ Main file for the quality check of an AI Solution
 This file correspond to the general value of the AI Quality Check page
 """
 
+from typing import Tuple
+
 import matplotlib.pyplot as plt
 import numpy as np
 import streamlit as st
@@ -21,10 +23,13 @@ class GlobalQuality:
         self.app = get_application()
         self.app.get_data()
 
-    def show_graph_score(self, score: dict) -> None:
+    def show_graph_score(self, score: dict[str, Tuple[int, int]]) -> None:
         """Method used to show a Eye-Catching Radial Bar Charts with all scores"""
         ring_colours = ["#2f4b7c", "#665191", "#a05195", "#d45087", "#f95d6a", "#ff7c43", "#ffa600"]
-        ring_labels = [f"   {score_elmt} ({score[score_elmt][0]}/{score[score_elmt][1]}) " for score_elmt in score]
+        ring_labels = [
+            f"   {score_elmt.replace('_', ' ')} ({score[score_elmt][0]}/{score[score_elmt][1]}) "
+            for score_elmt in score
+        ]
         fig = plt.figure(figsize=(10, 10), linewidth=10, edgecolor="#ffffff", facecolor="#ffffff")
         rect = [0.1, 0.1, 0.8, 0.8]
         # Add axis for radial backgrounds
@@ -66,7 +71,7 @@ class GlobalQuality:
         ax_polar.tick_params(axis="both", left=False, bottom=False, labelbottom=False, labelleft=True)
         st.pyplot(fig)
 
-    def show_global_score(self, score: dict) -> None:
+    def show_global_score(self, score: dict[str, Tuple[int, int]]) -> None:
         global_score = 0.0
         for _, (value, max_value) in score.items():
             global_score += value / max_value
